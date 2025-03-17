6cebc4208c7596dcede0e1896ef5793eb0d8a09e
Duc Lay Vo <duckisidoros@gmail.com>
2025-03-17 09:32:26 +0700

    The basic structure of the project

---
+++ frontend/main.py
@@ -0,0 +1,1 @@
+# Đây là file mã nguồn của frontend:
--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from kivy.app import App
-from kivy.uix.label import Label
+# Đây là file phân luồng chạy frontend và backend cùng lúc:
+import threading
+import os 
 
-class MyApp(App):
-    def build(self):
-        return Label(text="Hello, Kivy!")
+def backend():
+    os.system('python backend/main.py')
 
-if __name__ == "__main__":
-    MyApp().run()
+def frontend():
+    os.system('python frontend/main.py')
+
+if __name__ == '__main__':
+    backend_t=threading.Thread(target=backend)
+    frontend_t=threading.Thread(target=frontend)
+
+    backend_t.start()
+    frontend_t.start()
+
+    backend_t.join()
+    frontend_t.join()
