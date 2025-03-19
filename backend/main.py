c46f8c6ae0d46bcf67b7565c74cccdd14060fdc3
DuckIsidoros <duckisidoros@gmail.com>
2025-03-19 11:14:45 +0700

    Signup if using email

--- backend/main.py
+++ backend/main.py
@@ -2,16 +2,28 @@
 from flask import Flask, jsonify, request
 import requests, json
 import re, string
-
+import dns.resolver
+from firebase_admin import credentials, auth, initialize_app
 # This is the area for config information:
 app=Flask(__name__)
-auth="https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAFVY0tJ2mDjkmKZJ6MeO1JdeNBXDlpmAg"
+authe="https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAFVY0tJ2mDjkmKZJ6MeO1JdeNBXDlpmAg"
+cred=credentials.Certificate(".json")
+initialize_app(cred)
 # The format of auth is: "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=<API_FIREBASE_AUTHENTICATION_KEYS>"
 # Team Backend nếu muốn chạy code thì có thể copy key API của Firebase Console vào phần <>.
 
+def check_email():
+    try: 
+        mx_check=dns.resolver.resolve(domain, 'MX')
+        a_check=dns.resolver.resolve(domain, 'A')
+        return len(mx_check) > 0 and len(a_check) > 0
+    except (dns.resolver.resolve.NXDOMAIN, dns.resolver.NoAnswer):
+        return False
 @app.route('/sign-up', methods=['POST'])
 def signup():
     user=request.get_json() # the frontend team will send a json file which contain username and password to the backend team.
+    email=user.get('email')
+    password=user.get('password')
 
     # This block of code is used to check whether the input is invalid or not!
     if not user or not user.get('email') or not user.get('password') or not user.get('confirm_password'):
@@ -24,14 +36,15 @@ def signup():
         return jsonify({'message': 'Your email is in invalid form!'})
     if not any(char in string.punctuation for char in user.get('password')):
         return jsonify({'message': 'The password has to be have at least 1 special character!'})
-    
-    email=user.get('email')
-    password=user.get('password')
+    domain=email.split('@')[1]
+    domain=check_email(domain)
+    if not domain:
+        return jsonify({'message': 'Your domain isn't existed'})
     user={
-        'email': email,
-        'password': password,
+        'username': email,
+        'password': password
     }
-    user=requests.post(auth, json=user)
+    user=requests.post(authe, json=user)
     return jsonify({'message': 'Sucessfully!'})
 
 if __name__=="__main__":
