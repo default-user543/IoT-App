922f503a8a7f48b85cc077db1f325795881ea28f
Duc Lay Vo <duckisidoros@gmail.com>
2025-04-03 19:48:07 +0700

    Add test file and the database

--- backend/send_requests.py
+++ backend/send_requests.py
@@ -1,4 +1,4 @@
 import requests, json 
 
-response=requests.post("http://127.0.0.1:5000/check_location", json={"lat": 11.108313675344915, "lng": 106.6142607874415})
+response=requests.post("http://127.0.0.1:5000/check-location", json={"latitude": 11.109248409517255, "longitude": 106.6176708740449})
 print(response.json())
