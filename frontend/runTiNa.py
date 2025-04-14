e5216587c702f5d641ee7a24f969f1e9d027484e
TeniLeNguyen <104240758@student.vgu.edu.vn>
2025-04-15 00:57:18 +0700

    Tien 15/04 1AM

---
+++ frontend/runTiNa.py
@@ -0,0 +1,23 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager
+from homescreen import HomeScreen
+from menuscreen import MenuScreen
+
+class TiNaApp(App):
+    def build(self):
+        # Tạo ScreenManager để quản lý các màn hình
+        screen_manager = ScreenManager()
+
+        # Thêm màn hình HomeScreen
+        screen_manager.add_widget(HomeScreen(name='home'))
+
+        # Thêm màn hình MenuScreen
+        screen_manager.add_widget(MenuScreen(name='menu'))
+
+        # Đặt màn hình ban đầu là HomeScreen
+        screen_manager.current = 'home'
+
+        return screen_manager
+
+if __name__ == "__main__":
+    TiNaApp().run()
